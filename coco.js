53838d8bff8f38f6cc7b920b6766321ea443044d
Nikola Ivanović <nikola.ivanovic@gmail.com>
2019-12-17 15:13:24 +0100

    Create coco.js
    initial commit

---
+++ coco.js
@@ -0,0 +1,109 @@
+"use strict";
+
+var logEnabled = true;
+var logMode = "normal"; // normal, line
+let groupLevel = 0;
+const cocoFork = function(...parentArgs) {
+  const methods = [
+    "log",
+    "warn",
+    "info",
+    "debug",
+    "error",
+    "group",
+    "trace",
+    "table",
+    "groupEnd",
+    "groupCollapsed"
+  ];
+
+  const fork = function() {
+    fork.debug.apply(null, arguments);
+  };
+
+  methods.forEach(l => {
+    fork[l] = function(...args) {
+      if (!logEnabled) {
+        return fork;
+      }
+
+      if (l == "group" || l == "groupCollapsed") {
+        groupLevel++;
+        // console[l].apply(console, parentArgs);
+        // return;
+      }
+
+      if (l == "groupEnd") {
+        groupLevel--;
+        // console[l].apply(console, parentArgs);
+        // return;
+      }
+
+      // if (groupLevel == 0) {
+      //   args.splice(0, 0, ...parentArgs);
+      // }
+
+      // // if (
+      // //   l != "group" &&
+      // //   l != "groupCollapsed" &&
+      // //   l != "groupEnd" &&
+      // //   groupLevel > 0
+      // // ) {
+      // //   args.splice(0, parentArgs.length);
+      // // }
+
+      if (
+        l != "group" &&
+        l != "groupCollapsed" &&
+        l != "groupEnd" &&
+        groupLevel > 0
+      ) {
+        args.push(parentArgs);
+      } else {
+        args.splice(0, 0, ...parentArgs);
+      }
+
+      console[l].apply(console, args);
+
+      return fork;
+    };
+  });
+
+  fork.fork = function(...args) {
+    args.splice(0, 0, ...parentArgs);
+    return horisenLogFork(...args);
+  };
+
+  fork.begin = function (...args) {
+    const f = fork.fork(...args);
+    f.groupCollapsed();
+    return f;
+  }
+
+  fork.end = function () {
+    fork.groupEnd();
+  }
+
+  fork.block = function (blockName, cb) {
+    const forkBlock = fork.begin(blockName);
+    const ret = cb(forkBlock);
+    forkBlock.end();
+    return ret;
+  }
+
+  return fork;
+};
+
+const coco = cocoFork();
+
+coco.enable = function() {
+  logEnabled = true;
+  return horisenLog;
+};
+
+coco.disable = function() {
+  logEnabled = false;
+  return horisenLog;
+};
+
+export default horisenLog;
